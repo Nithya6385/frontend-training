305c862cd95a16159ff6f849295ea0742412e6a3
unknown <nithyasundar9103@gmail.com>
2022-10-13 17:09:41 -0700

    news_app

--- 10-10-22/output/news_app/js/script.js
+++ 10-10-22/output/news_app/js/script.js
@@ -21,11 +21,12 @@ function addDetails(key,value,index=null) {
     localStorage.setItem("newspaper",JSON.stringify(list_1));
  }
  namedisplay();
-        function namedisplay() {
-            let ele=" ";
-            let local_list= JSON.parse(localStorage.getItem("newspaper"));
-            for (let i = 0; i < local_list.length; i++) {
-                ele=ele+"<li> <a href='home.html?idname="+local_list[i].idname +"'target='_blank'>"+local_list[i].idname+"</a></li>";  
-            }
-            document.getElementById("idlist").innerHTML=ele;
-        }
+
+function namedisplay() {
+    let ele=" ";
+    let local_list= JSON.parse(localStorage.getItem("newspaper"));
+    for (let i = 0; i < local_list.length; i++) {
+        ele=ele+"<h2> <a href='home.html?title="+local_list[i].title +"'target='_blank'>"+local_list[i].title+"</a></h2>";  
+    }
+    document.getElementById("idlist").innerHTML=ele;
+}
---
+++ D28-10-10-22/output/news_app/js/script.js
@@ -0,0 +1,262 @@
+let news_arr=[
+    {
+        id:1,
+        title:"Ukraine war: Zelensky calls for more sanctions after 'new wave of terror'",
+        country:"Europe",
+        catagory:"war",
+        content:"At least 19 people were killed and scores more injured as Russian missiles hit regions across the country. Defiant, he said the attacks will only delay our recovery a little.Following more strikes on Tuesday, Mr Zelensky called on the West to find new ways to apply political pressure to Russia and support Ukraine.  The calls came after he met the G7 group of nations for emergency virtual talks on Tuesday.",        
+        short_content:"President Zelensky has urged countries to hit Russia with more sanctions in response to 'a new wave of terror' after Monday's attacks on Ukraine. ",
+        news_type:"national",
+        author:"Oliver Slow",
+        image:"https://ichef.bbci.co.uk/news/976/cpsprodpb/116E3/production/_127059317_28e23852e9c034705928de2e375daab4fd096396.jpg.webp",
+        date:"12-10-22",
+    },
+    {
+        id:2,
+        title:"US returns 23 looted artefacts to Nigeria",
+        country:"Africa",
+        catagory:"art",
+        content:"The United States has returned 23 pieces of looted artefacts to Nigeria. The Benin Bronzes were handed over to a Nigerian delegation at a ceremony on Tuesday in Washington. Nigerias Minister of Information and Culture Lai Mohammed, who received the artefacts.",
+        short_content:"A London museum says it has agreed to return to Nigeria artefacts looted in the 19th Century from the Kingdom of Benin. ",
+        news_type:"National",
+        author:"Oliver Slow",
+        image:"https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/vivo/live/images/2022/10/12/a7275605-d858-4a08-a2ed-832a2f16d053.jpg",
+        date:"11-10-22",
+    },
+    {
+        id:3,
+        title:"At least 33 killed in Nepal flooding and landslides",
+        country:"Nepal",
+        catagory:"weather",
+        content:"At least 33 people have died in floods and landslides across western Nepal in the past week, local media reported.The worst of the monsoon rains hit Karnali province in the north-west, where thousands of residents were evacuated, officials said. Hundreds of homes have been damaged in the avalanches and flooding. At least 22 people are still missing across the province and scores more have injuries.",
+        short_content:"A Maharashtra village switches off TV and internet for a few hours daily - and the results are worth it. ",
+        news_type:"National",
+        author:"Oliver Slow",
+        image:"https://ichef.bbci.co.uk/news/976/cpsprodpb/13303/production/_127059587_unicefnepa.png.webp",
+        date:"11-10-22",
+    },
+    {
+        id:4,
+        title:"BRAZIL ON FIRE EPISODE 6: AMAZON UP IN SMOKE",
+        country:"Africa",
+        catagory:"weather ",
+        content:"Brazil's war against Indigenous peoples is as old as Brazil itself, but Bolsonaro’s administration has sought to accelerate the destruction of the Amazon and its peoples.",
+        short_content:"It follows a decade-long investigation with several countries into lucrative business contracts.",
+        news_type:"National",
+        author:"Oliver Slow",
+        image:"https://i0.wp.com/therealnews.com/wp-content/uploads/2022/09/GettyImages-1187379783.jpg?resize=1200%2C900&ssl=1",
+        date:"30-09-22",
+    },
+    {
+        id:5,
+        title:"U.S. Treasury Secretary Yellen to visit India in November",
+        country:"U.S",
+        catagory:"business",
+        content:"Finance Minister Nirmala Sitharaman held bilateral talks with her American counterpart, Treasury Secretary Janet Yellen, during which Ms. Yellen announced that she would visit India in November.",
+        short_content:" Janet Yellen has been confirmed as the first ever female US treasury secretary in a Senate vote.",
+        news_type:"International",
+        author:"Oliver Slow",
+        image:"https://th-i.thgim.com/public/news/national/nmyw54/article65999687.ece/alternates/FREE_1200/yellen%20sitharaman%20washington.jpg",
+        date:"12-10-22",
+    },
+    {
+        id:6,
+        title:"Obituary: Angela Lansbury",
+        country:"U.S",
+        catagory:"art",
+        content:"Her role as the genteel Jessica Fletcher made Dame Angela Lansbury one of Britain's favourite exports. The success of the television series Murder She Wrote was a high point in a showbusiness career lasting more than seven decades. It took her from Broadway to Hollywood and then international fame on the small screen.",
+        short_content:"Her role as the genteel Jessica Fletcher made Dame Angela Lansbury one of Britain's favourite exports. ",
+        news_type:"national",
+        author:"Oliver Slow",
+        image:"https://ichef.bbci.co.uk/news/976/cpsprodpb/BA84/production/_96084774_mediaitem96084773.jpg.webp",
+        date:"11-10-22",
+    },
+    {
+        id:7,
+        title:"China's electric car market is booming but can it last?",
+        country:"China",
+        catagory:"business",
+        content:"Five years ago, the city revealed plans to ban the introduction of fossil fuel-powered taxis. Today, thousands of the cars run on batteries instead. And the drivers of these electric vehicles (EVs) don't have to worry about wasting time at charging stations, either.",
+        short_content:" Five years ago, the city revealed plans to ban the introduction of fossil fuel-powered taxis.",
+        news_type:"national",
+        author:"Oliver Slow",
+        image:"https://ichef.bbci.co.uk/news/976/cpsprodpb/E6D5/production/_126639095_04bd0b33-1c45-4fb4-9a12-5c29990a8b31.jpg",
+        date:"4_9_22",
+    },
+    {
+        id:8,
+        title:"Fat Bear Week winner crowned after cheating scandal",
+        country:"Canada",
+        catagory:"weather",
+        content:`After a six-day battle and vast amounts of salmon, Fat Bear Week has a winner. Brown bear 747, tipping the scales at an estimated 1,400lbs (635kg), won 68,105 votes, beating Bear 901 with 56,876 votes.",
+        short_content:"Brown bear 747, tipping the scales at an estimated 1,400lbs (635kg), won 68,105 votes, beating Bear 901 with 56,876 votes.
+
+        The result marks an end to Fat Bear Week 2022, which saw people from around the world vote for the fattest bruin at Alaska's Katmai National Park.
+        
+        The chow-down spotlights the park's famous brown bears as they feed in preparation for winter hibernation.
+        
+        "Ultimately, 747 ripped apart the salmon - and the competition - to seize his 2nd crown as the 2022 Champion," the park tweeted after Tuesday's vote.
+        
+        But Bear 747 nearly didn't make it to the final because of an unprecedented case of voter fraud in the semi-finals that was quickly corrected by officials. `,
+        news_type:"International",
+        author:"Oliver Slow",
+        image:"https://ichef.bbci.co.uk/news/976/cpsprodpb/6F3B/production/_127057482_52403134745_8b6642424f_c.jpg",
+        date:"11-10-22",
+    },
+    
+    {
+        id:9,
+        title:"Tales of bloody Iran protest crackdown at border crossing",
+        country:"Iran",
+        catagory:"",
+        content:"Soran raises two fingers to his left temple and motions firing a gun. 'If you talk, they will put a bullet in your head,' he tells me. Around us, the Iranian mountains reach skywards. We're at a bus station in Penjwen, a town in Iraq's northern Kurdistan region that is close to the crossing post on the border with Iran.",
+        short_content:" ",
+        news_type:"national",
+        author:"Oliver Slow",
+        image:"https://ichef.bbci.co.uk/news/976/cpsprodpb/118C2/production/_127047817_busstation2.jpg.webp",
+        date:"12-10-22",
+    },
+    {
+        id:10,
+        title:"Should Tomori have been sent off?",
+        country:"India",
+        catagory:"sports",
+        content:"",
+        short_content:" ",
+        news_type:"National",
+        author:"Oliver Slow",
+        image:"https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/vivo/live/images/2022/10/12/0f86aef5-9ef1-4a8f-bd12-3ec4925a0332.jpg",
+        date:"10-10-22",
+    },
+    {
+        id:11,
+        title:"Asia chipmaker shares slide after US curbs on China",
+        country:"China",
+        catagory:"sports",
+        short_content:"Chelsea see of AC Milan after Tomori red card Chelsea see of AC Milan after Tomori red card",
+        content:"Chelsea enjoyed a comfortable victory at AC Milan after former Blues defender Fikayo Tomori received an early red card. ",
+        news_type:"National",
+        author:"Oliver Slow",
+        image:"https://ichef.bbci.co.uk/onesport/cps/800/cpsprodpb/13033/production/_127057877_gettyimages-1432632383.jpg",
+        date:"12-10-22",
+    },
+    {
+        id:12,
+        title:"The 'queen of two courts' whose brilliance was ignored",
+        country:"US",
+        catagory:"sports",
+        content:`It was March 1976 and the host was giving a speech about the latest inductee. You probably won't know her name.
+
+        Ora Washington was a champion, a star of two sports, but prejudice stopped her competing for the biggest prizes of the day. Her sporting career spanned three decades of change in her native United States, but change didn't come quickly enough.
+        
+        Washington retired from tennis and basketball in the 1940s. In the mid-1970s a new generation started to dig deeper into her story. Hence the gathering in New York.
+        
+        As the host finished introducing Washington to those gathered for that glamorous occasion, they started on something new: an apology.
+        
+        Washington wasn't there. There was a chair placed out on the stage for her, empty. The host said they were sorry but they hadn't been able to track her down.`,
+        short_content:"At a glitzy downtown New York hotel, the recently founded Black Athletes Hall of Fame was holding its annual ceremony.",
+        news_type:"International",
+        author:"Oliver Slow",
+        image:"https://ichef.bbci.co.uk/onesport/cps/800/cpsprodpb/D633/production/_126953845_col-blockson-oramaewashingtonc1939_photobyjohnwmosley_blocksoncollectiontul.jpg",
+        date:"11-10-22",
+    },
+    {
+        id:13,
+        title:"Cough-syrup scandal: How did it end up in The Gambia?",
+        country:"India",
+        catagory:"business",
+        content:`This happens when bacteria change over time and become resistant to drugs that are supposed to defeat them and cure the infections they cause.
+
+        Such resistance directly caused 1.27 million deaths worldwide in 2019, according to The Lancet, a medical journal. Antibiotics - which are considered to be the first line of defence against severe infections - did not work on most of these cases.This happens when bacteria change over time and become resistant to drugs that are supposed to defeat them and cure the infections they cause.
+
+        Such resistance directly caused 1.27 million deaths worldwide in 2019, according to The Lancet, a medical journal. Antibiotics - which are considered to be the first line of defence against severe infections - did not work on most of these cases.`,
+        short_content:"The deaths of nearly 70 children in The Gambia, linked to cough syrups made in India, are being investigated amid concerns about effective regulation of the manufacture and trade in medicines. ",
+        news_type:"International",
+        author:"Oliver Slow",
+        image:"https://ichef.bbci.co.uk/news/976/cpsprodpb/1186C/production/_126988717_capture.jpg.webp",
+        date:"12-10-22",
+    },
+    {
+        id:14,
+        title:"India facing a pandemic of antibiotics-resistant superbugs",
+        country:"India",
+        catagory:"business",
+        content:`This happens when bacteria change over time and become resistant to drugs that are supposed to defeat them and cure the infections they cause.
+
+        Such resistance directly caused 1.27 million deaths worldwide in 2019, according to The Lancet, a medical journal. Antibiotics - which are considered to be the first line of defence against severe infections - did not work on most of these cases.`,
+        short_content:"At the 1,000-bed not-for-profit Kasturba Hospital in the western Indian state of Maharashtra, doctors are grappling with a rash of antibiotic-resistant 'superbug infections'.",
+        news_type:"International",
+        author:"Oliver Slow",
+        image:"https://ichef.bbci.co.uk/news/976/cpsprodpb/765A/production/_126889203_c20eb582-3914-401e-989b-70be81181fbb.jpg.webp",
+        date:"12-10-22",
+    },
+    {
+        id:15,
+        title:"Amitabh Bachchan birthday",
+        country:"India",
+        catagory:"art",
+        content:`Prime Minister Narendra Modi joined scores of other prominent figures in wishing the veteran actor on Twitter. PM Modi said Bachchan "is one of India's most remarkable film personalities who has enthralled and entertained audiences across generations".
+
+        A retrospective exhibition of his rare pictures and his early films is being shown in India to mark the occasion.
+        
+        Beginning on Saturday, 8 October, a first-of-its kind festival of Bachchan's landmark early films, organised by the non-profit Film Heritage Foundation, is being held held over four days at 22 cinemas in 17 cities across India.`,
+        short_content:" Amitabh Bachchan birthday: The Bollywood superstar as you've never seen him before",
+        news_type:"national",
+        author:"Oliver Slow",
+        image:"https://ichef.bbci.co.uk/news/976/cpsprodpb/E9C0/production/_126904895_9fcec355-0350-4360-a79b-d83844c0eb8f.jpg.webp",
+        date:"11-10-22",
+    },
+    {
+        id:16,
+        title:"India village goes offline daily to help people talk",
+        country:"India",
+        catagory:"art",
+        content:`A siren goes off at 7pm every evening in Vadgaon village in Sangli district, an indication to all residents to switch off their TV sets and mobile phones.
+
+        The two instruments of "addiction" can be switched on when the village council sounds the siren again at 8.30pm.
+        
+        "We decided at the village meeting on 14 August - the eve of India's Independence Day - that we needed to stop this addiction," Vijay Mohite, president of the village council, told BBC Hindi. "From the next day, all television sets and mobiles were shut down when the siren went off."
+        
+        Vadgaon has a population of about 3,000 people, made up largely of farmers and sugar mill workers.
+        
+        Mr Mohite said children had become dependent on TV and mobile phones for online classes during the Covid-19 pandemic. As educational institutions reopened this year, children returned to regular classes in schools and colleges.`,
+        short_content:"A village in India's Maharashtra state has declared 'independence' from two modern-day addictions - television and mobile internet. At least, for a couple of hours every day. ",
+        news_type:"national",
+        author:"Oliver Slow",
+        image:"https://ichef.bbci.co.uk/news/976/cpsprodpb/3D42/production/_127028651_whatsappimage2022-10-10at9.10.35am.jpg.webp",
+        date:"10-10-22",
+    },
+    {
+        id:17,
+        title:"Angry residents rename Agra areas 'smelly town'",
+        country:"India",
+        catagory:"business",
+        content:`Residents of Shahganj and Jagdishpura localities blame an unfinished road for causing waterlogging and traffic jams.
+
+        The northern city is home to the Taj Mahal, India's most famous monument, and is a big draw for tourists.
+        
+        But residents in the two areas complain that they have to live with filth and grime in their neighbourhoods.
+        
+        They accuse the authorities of not fixing their problems despite repeated complaints.
+        
+        A spokesperson for Baby Rani Maurya, a lawmaker under whose constituency parts of the road come under, said that steps were being taken to start work on the road.`,
+        short_content:"Some angry residents in India's Agra city have temporarily renamed housing complexes as 'gutter colony' or 'smelly town' to protest against civic issues. ",
+        news_type:"International",
+        author:"Oliver Slow",
+        image:"https://ichef.bbci.co.uk/news/976/cpsprodpb/5264/production/_127029012_agra3.jpg.webp",
+        date:"12-10-22",
+    },
+    
+]
+
+localStorage.setItem('newspaper',JSON.stringify(news_arr));
+
+let list=JSON.parse(localStorage.getItem("newspaper"));
+for(let i=0; i<list.length;i++){
+    list[i].title="<a target='_blank' href='view.html?id="+list[i].id +"'>"+list[i].title+"</a>"
+    document.getElementById("title"+i).innerHTML=list[i].title;
+    document.getElementById("image"+i).src=list[i].image;
+   // document.getElementById("short_content"+i).innerHTML=list[i].short_content;
+    
+}
+
